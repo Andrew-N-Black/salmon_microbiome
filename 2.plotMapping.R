5d4268f46f78630d631efde29d6e37693bb95e86
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2025-07-21 10:08:28 -0700

    Update 2.plotMapping.R

--- 2.plotMapping.R
+++ 2.plotMapping.R
@@ -2,3 +2,4 @@ library(ggplot2)
 decon_results_mapping <- read.csv("~/Library/CloudStorage/Box-Box/Salmon_Microbiome/Files/decon_results_mapping.csv")
 
 ggplot(decon_results_mapping , aes(x=reorder(ID,PercentMapped), y = PercentMapped,fill=enteritis)) + geom_bar(stat = "identity") + theme_bw() + theme(axis.text.x = element_text(angle = 45)) + ylab("Salmon Mapping Rate %") + xlab("Sample") + ylim(c(0,100))+ scale_fill_manual(values=c("black","cadetblue","grey","green4"))+theme(legend.title=element_blank())+ theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1),text = element_text(size = 6))
+
