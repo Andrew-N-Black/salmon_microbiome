1f0c3002cf1628aee6d75372e727dc95968cbe54
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2025-08-14 11:30:17 -0700

    Create 4.phyloseq.R

---
+++ 4.phyloseq.R
@@ -0,0 +1,8 @@
+#Used files from :/nfs4/BIOMED/Arnold_Lab/projects/BLACK/OUT_ALab_0006_decon/qiime2/input/
+
+
+phyloseq_object <- qza_to_phyloseq(
++     features = "~/input/table.qza",
++     taxonomy = "~/input/taxonomy.qza",
++     metadata = "~/metadata.tsv" # QIIME 2 metadata file
++ )
