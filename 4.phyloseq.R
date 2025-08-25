e1414d7946a9c597be636997c4ba8045eff97380
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2025-08-25 13:00:57 -0700

    Update 4.phyloseq.R

--- 4.phyloseq.R
+++ 4.phyloseq.R
@@ -1,8 +1,9 @@
 #Used files from :/nfs4/BIOMED/Arnold_Lab/projects/BLACK/OUT_ALab_0006_decon/qiime2/input/
 
 
-phyloseq_object <- qza_to_phyloseq(
-+     features = "~/input/table.qza",
-+     taxonomy = "~/input/taxonomy.qza",
-+     metadata = "~/metadata.tsv" # QIIME 2 metadata file
-+ )
+phyloseq_object<-qza_to_phyloseq(features = "~/SMB_n61/qiime2/input/table.qza",taxonomy = "~/SMB_n61/qiime2/input/taxonomy.qza",metadata = "~/SMB_n61/input/metadata61.tsv")
+#phyloseq-class experiment-level object
+#otu_table()   OTU Table:         [ 4328 taxa and 61 samples ]
+#sample_data() Sample Data:       [ 61 samples by 5 sample variables ]
+#tax_table()   Taxonomy Table:    [ 4328 taxa by 7 taxonomic ranks ]
+
