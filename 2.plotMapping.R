e7d8876afd894cb4b1b80e9d36d5e2f8c223c98a
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2025-10-02 15:12:31 -0700

    Update 2.plotMapping.R

--- 2.plotMapping.R
+++ 2.plotMapping.R
@@ -6,5 +6,4 @@ decon_results_mapping <- read.csv("~/Library/CloudStorage/Box-Box/Salmon_Microbi
 #remove samples with NA under ASE
 df_cleaned <- decon_results_mapping %>% drop_na(ASE)
 
-ggplot(df_cleaned , aes(x=reorder(ID,PercentMapped), y = PercentMapped,fill=enteritis)) + geom_bar(stat = "identity") + theme_bw() + theme(axis.text.x = element_text(angle = 45)) + ylab("Salmon Mapping Rate %") + xlab("Sample") + ylim(c(0,100))+ scale_fill_manual(values=c("black","cadetblue","grey","green4"))+theme(legend.title=element_blank())+ theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1),text = element_text(size = 6))
-
+ggplot(df_cleaned , aes(x=reorder(ID,PercentMapped), y = PercentMapped,fill=ASE)) + geom_bar(stat = "identity") + theme_bw(base_size = 14)  + ylab("Salmon Mapping Rate %") + xlab("Sample") + ylim(c(0,100))+ scale_fill_brewer(palette = "Dark2",name = "ASE")+theme(axis.ticks.x = element_blank(),axis.text.x = element_blank())
