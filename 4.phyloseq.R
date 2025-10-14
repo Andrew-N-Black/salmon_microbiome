5ed252e7f0b898e2d3214e6e1bee7f2a08ae089e
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2025-10-14 14:11:18 -0700

    Update 4.phyloseq.R

--- 4.phyloseq.R
+++ 4.phyloseq.R
@@ -5,6 +5,8 @@ library(microViz)
 library(ggplot2)
 library(tibble)
 library(betareg)
+library(dplyr)
+library(reshape2)
 
 
 phyloseq_object<-qza_to_phyloseq(features = "~/SMB_n61/qiime2/input/table.qza",taxonomy = "~/SMB_n61/qiime2/input/taxonomy.qza",metadata = "~/SMB_n61/input/metadata61_ext.txt")
@@ -72,7 +74,29 @@ ggplot(ps.melt_sum, aes(x = Sample, y = Abundance, fill = Phylum)) +
     labs(x="", y="%") +
     facet_wrap(~ASE, scales= "free_x", nrow=1) +
     theme_q2r()+scale_fill_brewer(palette = "Dark2",name = "Phylum")+theme(axis.ticks.x = element_blank(),axis.text.x = element_blank())
-ggsave("~/Figure_2A.svg")
+
+#Abundance by Class
+glom <- tax_glom(ps_rarefied, taxrank = 'Class', NArm = FALSE)
+#Melt and merge dataframe to work with ggplot2
+ps.melt <- psmelt(glom)
+#Set as character
+ps.melt$Class <- as.character(ps.melt$Class)
+
+ps.melt <- ps.melt %>%group_by(ASE, Class) %>% mutate(median=median(Abundance))
+keep <- unique(ps.melt$Class[ps.melt$median > 2.5])
+ps.melt$Class[!(ps.melt$Class %in% keep)] <- "< 2.5%"
+ps.melt_sum <- ps.melt %>% group_by(Sample,ASE,Class) %>% summarise(Abundance=sum(Abundance))
+
+ggplot(ps.melt_sum, aes(x = Sample, y = Abundance, fill = Class)) + 
+    geom_bar(stat = "identity", aes(fill=Class)) + 
+    labs(x="", y="%") +
+    facet_wrap(~ASE, scales= "free_x", nrow=1) +
+    theme_q2r()+scale_fill_brewer(palette = "Dark2",name = "Class")+theme(axis.ticks.x = element_blank(),axis.text.x = element_blank())
+ggsave("~/Figure_2.svg")
+
+
+
+
 #Abundance by genus
 
 
@@ -90,7 +114,6 @@ ggplot(ps.melt_sum, aes(x = Sample, y = Abundance, fill = Genus)) +
     labs(x="", y="%") +
     facet_wrap(~ASE, scales= "free_x", nrow=1) +
     theme_q2r()+scale_fill_brewer(palette = "Dark2",name = "Genus")+theme(axis.ticks.x = element_blank(),axis.text.x = element_blank())
-ggsave("~/Figure_2B.svg")
 
 
 #Ordination
