c8572456cdc3c44ffd6afbedc3281bfc1764da62
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2025-12-16 19:04:27 -0800

    Update 4.phyloseq.R

--- 4.phyloseq.R
+++ 4.phyloseq.R
@@ -274,85 +274,60 @@ sigtab = res[(res$padj < alpha), ]
 sigtab = cbind(as(sigtab, "data.frame"), as(tax_table(ps_filtered)[rownames(sigtab), ], "matrix"))
 View(sigtab)
 
-###PLOT single taxa from DeSeq
-target_asv<-"febf0b4ade55c5046e9bbc5c25bb4ef4"
-ps_sig <- prune_taxa(target_asv, ps_filtered) 
-df <- psmelt(ps_sig)
-ggplot(df, aes(x = ASE, y = Abundance, fill = ASE)) +
-    geom_boxplot(outlier.shape = NA,aes(fill=ASE)) +
-    geom_jitter(width = 0.2, size = 1, alpha = 0.6) +
-    facet_wrap(~OTU, scales = "free_y")+ggtitle("Mesomycoplasma moatsii")+scale_fill_brewer(palette = "Dark2")+labs(fill = "ASE")+theme_bw()+xlab("")
-
-
-target_asv<-"81a1706a3dc2fa4a573fca6c272332c2"
-ps_sig <- prune_taxa(target_asv, ps_filtered) 
-df <- psmelt(ps_sig)
-ggplot(df, aes(x = ASE, y = Abundance, fill = ASE)) +
-    geom_boxplot(outlier.shape = NA,aes(fill=ASE)) +
-    geom_jitter(width = 0.2, size = 1, alpha = 0.6) +
-    facet_wrap(~OTU, scales = "free_y")+ggtitle("Genus: Malacoplasma")+scale_fill_brewer(palette = "Dark2")+labs(fill = "ASE")+theme_bw()+xlab("")
+###PLOT single taxa from DeSeq and ANCONBC2
 
 target_asv<-"ce945369a663473cd641d04ae72b4418"#CONFIRMED
 ps_sig <- prune_taxa(target_asv, ps_filtered) 
 df <- psmelt(ps_sig)
-ggplot(df, aes(x = ASE, y = Abundance, fill = ASE)) +
-    geom_boxplot(outlier.shape = NA,aes(fill=ASE)) +
+ggplot(df, aes(x = percent_epithelium, y = Abundance, color = ASE)) +
+    geom_point(size = 5,shape=21,color="black",aes(fill=ASE)) +
     geom_jitter(width = 0.2, size = 1, alpha = 0.6) +
-    facet_wrap(~OTU, scales = "free_y")+ggtitle("Kingdom: Bacteria")+scale_fill_brewer(palette = "Dark2")+labs(fill = "ASE")+theme_bw()+xlab("")
+    facet_wrap(~OTU, scales = "free_y")+ggtitle("Kingdom: Bacteria")+scale_fill_brewer(palette = "Dark2")+theme_bw()+xlab("")+scale_color_brewer(palette = "Dark2")
+ggsave("~/a.svg")
 
->ce945369a663473cd641d04ae72b4418
-TACGGAGGGTGCAAGCGTTAATCGGAATTACTGGGCGTAAAGCGCGCGTAGGCGGTAATGTAAGTTAGAGGTGTAAGCCCATAGCTCAACTATGGAATTGCCTTTAAGACTGCGTTACTAGAATATAGGAGAGGATAGTGGAATTTCTAGTGTAGGAGTGGAATCTGTAGATACTAGAAGGAACACCAGAGGCGAAGGCGACTATCTGGACTATTATTGACGCTGAGGTGCGAAAGCGTGGGGAGCAAACAGG
-
-target_asv<-"c9b6f06a64809b4085cf1c5c680fc62b"
-ps_sig <- prune_taxa(target_asv, ps_filtered) 
-df <- psmelt(ps_sig)
-ggplot(df, aes(x = ASE, y = Abundance, fill = ASE)) +
-    geom_boxplot(outlier.shape = NA,aes(fill=ASE)) +
+target_asv<-"82819e0b6b0a7ba359661678cb034a42"#CONFIRMED
+ggplot(df, aes(x = percent_epithelium, y = Abundance, color = ASE)) +
+    geom_point(size = 5,shape=21,color="black",aes(fill=ASE)) +
     geom_jitter(width = 0.2, size = 1, alpha = 0.6) +
-    facet_wrap(~OTU, scales = "free_y")+ggtitle("Genus: Rhodoferax")+scale_fill_brewer(palette = "Dark2")+labs(fill = "ASE")+theme_bw()+xlab("")
+    facet_wrap(~OTU, scales = "free_y")+scale_fill_brewer(palette = "Dark2")+theme_bw()+xlab("")+scale_color_brewer(palette = "Dark2")+ggtitle("Carnobacterium inhibens")
+ggsave("~/b.svg")
 
-
-
- target_asv<-"82dece6e35540738ba450a0c3a90b5a0"#CONFIRMED
-ps_sig <- prune_taxa(target_asv, ps_filtered) 
-df <- psmelt(ps_sig)
-ggplot(df, aes(x = ASE, y = Abundance, fill = ASE)) +
-geom_boxplot(outlier.shape = NA,aes(fill=ASE)) +
-geom_jitter(width = 0.2, size = 1, alpha = 0.6) +
-facet_wrap(~OTU, scales = "free_y")+ggtitle("Serratia marcescens")+scale_fill_brewer(palette = "Dark2")+labs(fill = "ASE")+theme_bw()+xlab("")
-
-
-target_asv<-"d114fb4c335125128be28401522dd41a"
-ps_sig <- prune_taxa(target_asv, ps_filtered) 
-df <- psmelt(ps_sig)
-ggplot(df, aes(x = ASE, y = Abundance, fill = ASE)) +
-    geom_boxplot(outlier.shape = NA,aes(fill=ASE)) +
-    geom_jitter(width = 0.2, size = 1, alpha = 0.6) +
-    facet_wrap(~OTU, scales = "free_y")+ggtitle("Lactococcus lactis")+scale_fill_brewer(palette = "Dark2")+labs(fill = "ASE")+theme_bw()+xlab("")
 
 target_asv<-"bc56a7361c9a3b49f1f1c51874321e12"#CONFIRMED
 ps_sig <- prune_taxa(target_asv, ps_filtered) 
 df <- psmelt(ps_sig)
-ggplot(df, aes(x = ASE, y = Abundance, fill = ASE)) +
-    geom_boxplot(outlier.shape = NA,aes(fill=ASE)) +
+ggplot(df, aes(x = percent_epithelium, y = Abundance, color = ASE)) +
+    geom_point(size = 5,shape=21,color="black",aes(fill=ASE)) +
     geom_jitter(width = 0.2, size = 1, alpha = 0.6) +
-    facet_wrap(~OTU, scales = "free_y")+ggtitle("Kocuria rhizophila")+scale_fill_brewer(palette = "Paired")+labs(fill = "ASE")+theme_bw()+xlab("")
+    facet_wrap(~OTU, scales = "free_y")+scale_fill_brewer(palette = "Dark2")+theme_bw()+xlab("")+scale_color_brewer(palette = "Dark2")+ggtitle("Kocuria rhizophila")
+ggsave("~/c.svg")
 
 target_asv<-"2500422919f98bed627f3fd491e508a8"#CONFIRMED
 ps_sig <- prune_taxa(target_asv, ps_filtered) 
 df <- psmelt(ps_sig)
-ggplot(df, aes(x = ASE, y = Abundance, fill = ASE)) +
-    geom_boxplot(outlier.shape = NA,aes(fill=ASE)) +
+ggplot(df, aes(x = percent_epithelium, y = Abundance, color = ASE)) +
+    geom_point(size = 5,shape=21,color="black",aes(fill=ASE)) +
     geom_jitter(width = 0.2, size = 1, alpha = 0.6) +
-    facet_wrap(~OTU, scales = "free_y")+ggtitle("Aeromonas sobria")+scale_fill_brewer(palette = "Paired")+labs(fill = "ASE")+theme_bw()+xlab("")
+    facet_wrap(~OTU, scales = "free_y")+scale_fill_brewer(palette = "Dark2")+theme_bw()+xlab("")+scale_color_brewer(palette = "Dark2")+ggtitle("Aeromonas sobria")
+ggsave("~/d.svg")
 
-target_asv<-"82819e0b6b0a7ba359661678cb034a42"#CONFIRMED
+ target_asv<-"82dece6e35540738ba450a0c3a90b5a0"#CONFIRMED
 ps_sig <- prune_taxa(target_asv, ps_filtered) 
 df <- psmelt(ps_sig)
-ggplot(df, aes(x = ASE, y = Abundance, fill = ASE)) +
-    geom_boxplot(outlier.shape = NA,aes(fill=ASE)) +
+ggplot(df, aes(x = percent_epithelium, y = Abundance, color = ASE)) +
+    geom_point(size = 5,shape=21,color="black",aes(fill=ASE)) +
     geom_jitter(width = 0.2, size = 1, alpha = 0.6) +
-    facet_wrap(~OTU, scales = "free_y")+ggtitle("Carnobacterium inhibens")+scale_fill_brewer(palette = "Paired")+labs(fill = "ASE")+theme_bw()+xlab("")
+    facet_wrap(~OTU, scales = "free_y")+scale_fill_brewer(palette = "Dark2")+theme_bw()+xlab("")+scale_color_brewer(palette = "Dark2")+ggtitle("Serratia marcescens")
+
+ggsave("~/e.svg")
+
+
+
+
+>ce945369a663473cd641d04ae72b4418
+TACGGAGGGTGCAAGCGTTAATCGGAATTACTGGGCGTAAAGCGCGCGTAGGCGGTAATGTAAGTTAGAGGTGTAAGCCCATAGCTCAACTATGGAATTGCCTTTAAGACTGCGTTACTAGAATATAGGAGAGGATAGTGGAATTTCTAGTGTAGGAGTGGAATCTGTAGATACTAGAAGGAACACCAGAGGCGAAGGCGACTATCTGGACTATTATTGACGCTGAGGTGCGAAAGCGTGGGGAGCAAACAGG
+
+
 
 
 
