b69d4a43c7c99b64112444a30832c82a2f48b118
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2025-12-22 12:24:53 -0800

    Update ampliseq.sh

--- 2.BionformaticProcessing:nf-core/ampliseq.sh
+++ 2.BionformaticProcessing:nf-core/ampliseq.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+#SBATCH -A core
+#SBATCH -p core
+#SBATCH -N 1
+#SBATCH -n 30
+#SBATCH --mem=100G
+#SBATCH --job-name=SM_nfcore
 
+export SINGULARITY_TMPDIR=/scratch
+nextflow run /local/cqls/software/nextflow/assets/nf-core-ampliseq_2.14.0/2_14_0/ -profile singularity -c nextflow.config -params-file  nf-params.json -resume
