94471db9522253179dd2720ac90ad7a8437390d4
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2025-12-22 12:21:12 -0800

    Delete 2.plotMapping.R

--- 2.plotMapping.R
+++
@@ -1,9 +0,0 @@
-library(ggplot2)
-library(tidyr)
-
-#load file
-decon_results_mapping <- read.csv("~/Library/CloudStorage/Box-Box/Salmon_Microbiome/Files/decon_results_mapping.csv")
-#remove samples with NA under ASE
-df_cleaned <- decon_results_mapping %>% drop_na(ASE)
-
-ggplot(df_cleaned , aes(x=reorder(ID,PercentMapped), y = PercentMapped,fill=ASE)) + geom_bar(stat = "identity") + theme_bw(base_size = 14)  + ylab("Salmon Mapping Rate %") + xlab("Sample") + ylim(c(0,100))+ scale_fill_brewer(palette = "Dark2",name = "ASE")+theme(axis.ticks.x = element_blank(),axis.text.x = element_blank())
