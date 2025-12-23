3c1c0b9446cf72cf3707fc96b155ea5d00f6f5cc
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2025-12-22 19:53:39 -0800

    Create 01_phyloseq.R

---
+++ 02_TaxonomicProfiling/01_phyloseq.R
@@ -0,0 +1,34 @@
+library(phyloseq)
+library(qiime2R)
+library(microViz)
+library(ggplot2)
+library(tibble)
+library(dplyr)
+library(reshape2)
+library(vegan)
+
+hyloseq_object<-qza_to_phyloseq(features = "~/SMB_n61/qiime2/input/table.qza",taxonomy = "~/SMB_n61/qiime2/input/taxonomy.qza",metadata = "~/SMB_n61/input/metadata61_ext.txt")
+#phyloseq-class experiment-level object
+#otu_table()   OTU Table:         [ 4328 taxa and 61 samples ]
+#sample_data() Sample Data:       [ 61 samples by 14 sample variables ]
+#tax_table()   Taxonomy Table:    [ 4328 taxa by 7 taxonomic ranks ]
+
+ps_MC <- subset_taxa(phyloseq_object, !Order %in% c('Chloroplast', 'Mitochondria'))
+#phyloseq-class experiment-level object
+#otu_table()   OTU Table:         [ 4212 taxa and 61 samples ]
+#sample_data() Sample Data:       [ 61 samples by 14 sample variables ]
+#tax_table()   Taxonomy Table:    [ 4212 taxa by 7 taxonomic ranks ]
+
+#Remove NA kingdom assignments
+ps_filtered <- subset_taxa(ps_MC, !is.na(Kingdom))
+#phyloseq-class experiment-level object
+#otu_table()   OTU Table:         [ 4193 taxa and 61 samples ]
+#sample_data() Sample Data:       [ 61 samples by 14 sample variables ]
+#tax_table()   Taxonomy Table:    [ 4193 taxa by 7 taxonomic ranks ]
+
+#Rarefied
+ps_rarefied = rarefy_even_depth(ps_filtered)
+#phyloseq-class experiment-level object
+#otu_table()   OTU Table:         [ 1583 taxa and 61 samples ]
+#sample_data() Sample Data:       [ 61 samples by 14 sample variables ]
+#tax_table()   Taxonomy Table:    [ 1583 taxa by 7 taxonomic ranks ]
