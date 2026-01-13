95562a125fa242ad5f9714d6cec81565349e979c
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2026-01-13 14:02:46 -0800

    Update SI.R

--- SI.R
+++ SI.R
@@ -8,3 +8,17 @@ df_cleaned <- decon_results_mapping %>% drop_na(ASE)
 
 ggplot(df_cleaned , aes(x=reorder(ID,PercentMapped), y = PercentMapped,fill=ASE)) + geom_bar(stat = "identity") + theme_bw(base_size = 14)  + ylab("Salmon Mapping Rate %") + xlab("Sample") + ylim(c(0,100))+ scale_fill_brewer(palette = "Dark2",name = "ASE")+theme(axis.ticks.x = element_blank(),axis.text.x = element_blank())
 
+#Figure 1b. Epithelium remaining
+
+ggplot(metadata, aes(y =epithelium_remaining, x=hatchery, fill = ASE)) +
+    geom_violin() +
+    labs(x = "Hatchery",
+         y = "# Samples",
+         fill = "ASE") +
+    theme_q2r()+scale_fill_brewer(palette = "Dark2",name = "ASE")+geom_boxplot(aes(x=hatchery, y=epithelium_remaining, fill=ASE), width=0.2)+geom_jitter(aes(x=hatchery, y=epithelium_remaining), width=0.1)
+
+#Figure 1c. Enteritis score vs hatchery
+ggplot(metadata, aes(x=hatchery,fill=enteritis)) +geom_bar(position = "stack")+labs(x = "Hatchery", y = "Count",fill = "hatchery") +theme_q2r()+scale_fill_brewer(palette = "Dark2",name = "Enteritis Score")
+
+#Figure 1d.Epithelium vs entiritus score scatter, hatchery shape
+ggplot(metadata, aes(x=enteritis,y=epithelium_remaining,color=ASE,shape=hatchery)) +geom_point(size=5,position = "jitter",aes(color=ASE))+labs(x = "Enteritis Score", y = "Epithilium Remaining",color = "ASE",shape="Hatchery") +theme_q2r()+scale_color_brewer(palette = "Dark2",name = "ASE")
