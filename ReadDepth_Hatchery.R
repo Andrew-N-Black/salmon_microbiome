46865ba3c6f7cbc8ed93b64a42e4b0b28174d6ca
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2026-01-13 15:46:33 -0800

    Update ReadDepth_Hatchery.R

--- ReadDepth_Hatchery.R
+++ ReadDepth_Hatchery.R
@@ -6,4 +6,8 @@ sample_data(ps_filtered)$total_reads <- total_reads_per_sample
 metadata<-sample_data(ps_filtered)
 
 #plot
-ggplot(metadata, aes(y =total_reads, x=hatchery, fill = ASE)) +labs(x = "Hatchery",y = "Total Read Depth/sample",fill = "ASE") +theme_q2r()+scale_fill_brewer(palette = "Dark2",name = "ASE")+geom_boxplot(aes(x=hatchery, y=total_reads, fill=ASE), width=0.2)+geom_jitter(aes(x=hatchery, y=total_reads), width=0.1)
+ggplot(metadata, aes(y =total_reads, x=hatchery)) +
+    geom_violin(fill="grey") +
+    labs(x = "Hatchery",
+         y = "Total Read Depth / sample") +
+    theme_q2r()+geom_boxplot(aes(x=hatchery, y=total_reads), width=0.2,fill="grey")+geom_jitter(aes(x=hatchery, y=total_reads), width=0.1)
