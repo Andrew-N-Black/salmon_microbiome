d3b736b2cd466531c34e3e4a545b2dca2a960c0c
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2026-01-27 19:09:46 -0800

    Update ReadDepth_Hatchery.R

--- ReadDepth_Hatchery.R
+++ ReadDepth_Hatchery.R
@@ -1,13 +1,19 @@
 
 #Summarize read depth / sample and save in metadata
+reads_per_sample <- sample_sums(ps_filtered)
 sample_data(ps_filtered)$total_reads <- total_reads_per_sample
 total_reads_per_sample <- sample_sums(ps_filtered)
 sample_data(ps_filtered)$total_reads <- total_reads_per_sample
 metadata<-sample_data(ps_filtered)
 
+#Reorder
+desired_order <- c("minter_creek","white_river", "south_santiam", "sandy", "willamette","round_butte")
+metadata$hatchery <- factor(metadata$hatchery, levels = desired_order)
+
 #plot
 ggplot(metadata, aes(y =total_reads, x=hatchery)) +
     geom_violin(fill="grey") +
-    labs(x = "Hatchery",
+    labs(x = "",
          y = "Total Read Depth / sample") +
-    theme_q2r()+geom_boxplot(aes(x=hatchery, y=total_reads), width=0.2,fill="grey")+geom_jitter(aes(x=hatchery, y=total_reads), width=0.1)
+    theme_classic(base_size = 14)+geom_boxplot(aes(x=hatchery, y=total_reads), width=0.2,fill="grey")+geom_jitter(aes(x=hatchery, y=total_reads), width=0.1)+theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1))
+
