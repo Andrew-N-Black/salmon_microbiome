eb08fca1f551aba98d66dac0a7db91f080253f67
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2026-02-10 14:48:13 -0800

    Update 01_phyloseq.R

--- 02_TaxonomicProfiling/01_phyloseq.R
+++ 02_TaxonomicProfiling/01_phyloseq.R
@@ -14,23 +14,22 @@ phyloseq_object<-qza_to_phyloseq(features = "~/SMB_n61/qiime2/input/table.qza",t
 #tax_table()   Taxonomy Table:    [ 4328 taxa by 7 taxonomic ranks ]
 
 ps_MC <- subset_taxa(phyloseq_object, Kingdom != "Eukaryota" & 
-                                Family != "Mitochondria" & 
-                                Class != "Chloroplast")
-#phyloseq-class experiment-level object
-#otu_table()   OTU Table:         [ 3726 taxa and 61 samples ]
-#sample_data() Sample Data:       [ 61 samples by 18 sample variables ]
-#tax_table()   Taxonomy Table:    [ 3726 taxa by 7 taxonomic ranks ]
-
-#Remove NA kingdom assignments
-ps_filtered <- subset_taxa(ps_MC, !is.na(Kingdom))
+                         Family != "Mitochondria" & 
+                         Class != "Chloroplast" & !is.na(Kingdom))
 #otu_table()   OTU Table:         [ 3726 taxa and 61 samples ]
 sample_data() Sample Data:       [ 61 samples by 17 sample variables ]
 tax_table()   Taxonomy Table:    [ 3726 taxa by 7 taxonomic ranks ]
 
+#Remove a single sample that now has a total read count below 10,000 (after removing Euk reads)
+sample_to_remove <- "WH21_10"
+ps <- prune_samples(!(sample_names(ps_MC) %in% sample_to_remove), ps_MC)
+#phyloseq-class experiment-level object
+#otu_table()   OTU Table:         [ 3726 taxa and 60 samples ]
+#sample_data() Sample Data:       [ 60 samples by 17 sample variables ]
+#tax_table()   Taxonomy Table:    [ 3726 taxa by 7 taxonomic ranks ]
 
-#NEED TO PICK BACK UP HERE!!!!!!2/5/26
 #Rarefied
-ps_rarefied = rarefy_even_depth(ps_filtered)
+ps_rarefied = rarefy_even_depth(ps,rngseed = 123)
 #phyloseq-class experiment-level object
 #otu_table()   OTU Table:         [ 1583 taxa and 61 samples ]
 #sample_data() Sample Data:       [ 61 samples by 14 sample variables ]
