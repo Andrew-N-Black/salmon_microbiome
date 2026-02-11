af7d1f9e4abc2e1f7dfb3687d179812713ae92a0
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2026-02-11 09:41:24 -0800

    Update 01_phyloseq.R

--- 02_TaxonomicProfiling/01_phyloseq.R
+++ 02_TaxonomicProfiling/01_phyloseq.R
@@ -28,9 +28,46 @@ ps <- prune_samples(!(sample_names(ps_MC) %in% sample_to_remove), ps_MC)
 #sample_data() Sample Data:       [ 60 samples by 17 sample variables ]
 #tax_table()   Taxonomy Table:    [ 3726 taxa by 7 taxonomic ranks ]
 
+#Parse phyloseq object for downstream analyses
+tax = as.data.frame(phyloseqCompanion::taxa.matrix(tax_table(ps)))
+otu = as.data.frame(phyloseqCompanion::otu.matrix(otu_table(ps)))
+meta = phyloseqCompanion::sample.data.frame(ps)
+
+#Sanity check phyloseq object
+head(tax)
+dim(tax)
+table(tax$Kingdom)
+sum(is.na(tax$Kingdom))
+table(tax$Phylum)
+table(tax$Class)
+table(tax$Order)
+table(tax$Family)
+
+#Add in ASV sequences from FASTA file
+dna <- readDNAStringSet("~/ASV_seqs.fasta")
+dna <- dna[taxa_names(ps)]
+ps <- merge_phyloseq(ps, dna)
+
+
 #Rarefied
 ps_rarefied = rarefy_even_depth(ps,rngseed = 123)
 #phyloseq-class experiment-level object
 #otu_table()   OTU Table:         [ 1583 taxa and 61 samples ]
 #sample_data() Sample Data:       [ 61 samples by 14 sample variables ]
 #tax_table()   Taxonomy Table:    [ 1583 taxa by 7 taxonomic ranks ]
+
+
+
+#Rename ASVs to make more human readable but preserve original name (KEY)
+# Assign each address a human readable sequence ID
+N = ntaxa(ps)
+KEY = data.frame(matrix(nrow = N, ncol = 2))
+colnames(KEY) = c("seq", "address")
+KEY$seq = (paste0("ASV", seq_len(N)))
+tax$address = rownames(tax)
+KEY$address = tax$address
+head(KEY) # This is the map off address to seqs
+
+
+
+
