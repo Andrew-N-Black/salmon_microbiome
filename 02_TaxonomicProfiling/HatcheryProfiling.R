e5d204d5b8efc26a3700919b685fcfa19f07fd28
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2026-02-13 12:56:16 -0800

    Update HatcheryProfiling.R

--- 02_TaxonomicProfiling/HatcheryProfiling.R
+++ 02_TaxonomicProfiling/HatcheryProfiling.R
@@ -1,7 +1,33 @@
 library(microViz)
-ps_rel = microbiome::transform(hr_phyloseq, "compositional")
-ps_min.10 <- microViz::tax_filter(ps_rel, min_prevalence = 0.10)
+ps_min.10 <- microViz::tax_filter(hr_phyloseq, min_prevalence = 0.10)
+Proportional min_prevalence given: 0.1 --> min 6/60 samples.
+ps_rel = microbiome::transform(ps_min.10, "compositional")
 
+#Fix taxa names
+ps_new <-ps_rel  %>%  tax_fix()
+myPal <- tax_palette(data = ps_new, rank = "Genus", n = 10, pal = "greenArmytage",add = c(Other = "white"))
+
+#Sort phyloseq object by hatchery name (ASE- then ASE+ hatcheries)
+desired_facet_order <- c("minter_creek","white_river", "south_santiam", "sandy", "willamette","round_butte")
+ps_new@sam_data$hatchery <- factor(ps_new@sam_data$hatchery, levels = desired_facet_order)
+
+#Genus level
+myPal <- tax_palette(data = ps_new, rank = "Genus", n = 15, pal = "greenArmytage",add = c(Other = "white"))
+
+#Genus level
+comp_barplot(ps=ps_new,tax_level = "Genus", n_taxa = 15, bar_width = 0.8,palette = myPal,merge_other=FALSE,facet_by = "hatchery") +labs(x = "", y = "Relative Abundance")+theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1))
+
+
+
+
+
+
+
+
+
+
+
+#OLD
 myPal <- tax_palette(data = ps_min.10, rank = "Genus", n = 10, pal = "greenArmytage",add = c(Other = "white"))
 
 #Genus level
