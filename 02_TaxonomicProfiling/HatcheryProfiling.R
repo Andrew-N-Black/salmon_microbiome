99b35582e8f3096a6cc01d7a0994ebe041e07ec3
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2026-02-16 09:18:52 -0800

    Update HatcheryProfiling.R

--- 02_TaxonomicProfiling/HatcheryProfiling.R
+++ 02_TaxonomicProfiling/HatcheryProfiling.R
@@ -22,6 +22,21 @@ comp_barplot(ps=ps_new,tax_level = "Phylum", n_taxa = 15, bar_width = 0.8,palett
 
 
 
+#Summarize read depth / sample and save in metadata
+read_counts <- sample_sums(hr_phyloseq)
+sample_data(hr_phyloseq)$TotalReads <- sample_sums(hr_phyloseq)
+metadata<-sample_data(hr_phyloseq)
+
+#Reorder
+desired_order <- c("minter_creek","white_river", "south_santiam", "sandy", "willamette","round_butte")
+metadata$hatchery <- factor(metadata$hatchery, levels = desired_order)
+
+#plot
+ggplot(metadata, aes(y =TotalReads, x=hatchery)) +
+    geom_violin(fill="grey") +
+    labs(x = "",
+         y = "log(Total Reads / Sample)") +
+    theme_classic(base_size = 14)+geom_boxplot(aes(x=hatchery, y=TotalReads), width=0.2,fill="grey")+geom_jitter(aes(x=hatchery, y=TotalReads), width=0.1)+theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1))+scale_y_log10()
 
 
 
