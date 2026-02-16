75f49cb2192fd1f6297aa9e0d7626ae14326b50b
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2026-02-16 15:23:16 -0800

    Update 05_permanova.R

--- 02_TaxonomicProfiling/05_permanova.R
+++ 02_TaxonomicProfiling/05_permanova.R
@@ -2,6 +2,8 @@
 
 #permanova using aitchison distance
 metadata <- data.frame(sample_data(ps_rarefied))
+desired_facet_order <- c("minter_creek","white_river", "south_santiam", "sandy", "willamette","round_butte")
+metadata$hatchery <- factor(metadata$hatchery, levels = desired_facet_order)
 
 #Center log transformed
 X_clr <- scale(log(X + 1), center = TRUE, scale = FALSE)
@@ -33,71 +35,15 @@ adonis2(aitchison_dist ~ hatchery, data = metadata)
 #Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
 
 
+#Plot betadisp distances by sorted hatchery
+p <- cbind(distance = as.numeric(dispersionB$distances),hatchery = metadata$hatchery,samples=rownames(metadata)) %>% as_tibble() %>% mutate(distance = as.numeric(distance)) 
+desired_facet_order <- c("minter_creek","white_river", "south_santiam", "sandy", "willamette","round_butte")
+p$hatchery <- factor(p$hatchery, levels = desired_facet_order)
 
-p <- cbind(distance = as.numeric(dispersionB$distances),Hatchery = metadata$hatchery,samples=rownames(metadata)) %>% as_tibble() %>% mutate(distance = as.numeric(distance)) %>% 
-    ggplot(aes(Hatchery, distance,color=Hatchery)) + 
+ggplot(p,aes(hatchery, distance,color=hatchery)) + 
     geom_boxplot() +
     theme_q2r()+xlab("Hatchery")+ylab("Distance from centroid")+scale_color_brewer(palette = "Dark2")+theme(axis.title.x = element_blank(),axis.text.x = element_blank(), axis.ticks.x = element_blank())
 
 ggsave("~/Figure_4_add.svg")
 
-
-
-
-dist_matrixB <- phyloseq::distance(ps_rarefied, method = "bray")
-dispersionB<-betadisper(dist_matrixB,group=ps_rarefied@sam_data$hatchery)
-#Warning message:
-#In betadisper(dist_matrixB, group = ps_rarefied@sam_data$hatchery) :
-#  some squared distances are negative and changed to zero
-
-permutest(dispersionB)
-#Permutation test for homogeneity of multivariate dispersions
-#Permutation: free
-#Number of permutations: 999
-
-#Response: Distances
-#          Df  Sum Sq  Mean Sq      F N.Perm Pr(>F)
-#Groups     5 0.25461 0.050922 1.0998    999  0.354
-#Residuals 54 2.50033 0.046302  
-
-#Run Permanova:
-adonis2(dist_matrixB ~ hatchery, data = metadata)
-
-#Permutation test for adonis under reduced model
-#Permutation: free
-#Number of permutations: 999
-
-#adonis2(formula = dist_matrixB ~ hatchery, data = metadata)
-#         Df SumOfSqs      R2      F Pr(>F)    
-#Model     5   8.7099 0.38919 6.8815  0.001 ***
-#Residual 54  13.6696 0.61081                  
-#Total    59  22.3796 1.00000                  
-#---
-#Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1      
-
-#Jaccard distance matrix-betadisp and permanova
-dist_matrixJ <- phyloseq::distance(ps_rarefied, method = "jaccard")
-
-dispersionJ<-betadisper(dist_matrixJ,group=ps_rarefied@sam_data$hatchery)
-permutest(dispersionJ)
-#Permutation test for homogeneity of multivariate dispersions
-#Permutation: free
-#Number of permutations: 999
-
-#Response: Distances
-#          Df  Sum Sq  Mean Sq      F N.Perm Pr(>F)
-#Groups     5 0.21547 0.043093 1.3545    999  0.268
-#Residuals 54 1.71796 0.031814
-
-adonis2(dist_matrixJ ~ ASE, data = metadata)
-#Permutation test for adonis under reduced model
-#Permutation: free
-#Number of permutations: 999
-
-#adonis2(formula = dist_matrixJ ~ hatchery, data = metadata)
-#         Df SumOfSqs      R2      F Pr(>F)    
-#Model     5   7.4452 0.29968 4.6215  0.001 ***
-#Residual 54  17.3988 0.70032                  
-#Total    59  24.8440 1.00000                  
-#---
-#Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1        
+    
