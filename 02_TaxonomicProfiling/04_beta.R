ea73de8ec89bed3ec393dde1c8558b33353c7d6d
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2026-02-16 13:05:37 -0800

    Update 04_beta.R

--- 02_TaxonomicProfiling/04_beta.R
+++ 02_TaxonomicProfiling/04_beta.R
@@ -5,6 +5,36 @@ library(ggplot2)
 library(tibble)
 library(dplyr)
 library(reshape2)
+library(ape)
+
+#Atchison distance PCOA
+X <- as(otu_table(phyloseq_object), "matrix")
+if (taxa_are_rows(phyloseq_object)) X <- t(X)  # samples x taxa
+
+X_clr <- scale(log(X + 1), center = TRUE, scale = FALSE)
+D_aitch <- dist(X_clr, method = "euclidean")
+
+pcoa <- ape::pcoa(D_aitch)
+
+var_expl <- 100 * pcoa$values$Relative_eig[1:2]
+print(round(100 * pcoa$values$Relative_eig[1:6], 2))
+
+pcoa_df <- as_tibble(pcoa$vectors[, 1:2], rownames = "sample") %>%
+  left_join(
+    sample_data(phyloseq_object) %>% data.frame() %>% rownames_to_column("sample"),
+    by = "sample"
+  )
+
+ggplot(pcoa_df, aes(Axis.1, Axis.2, color = ASE)) +
+  geom_point(size = 2.6, alpha = 0.9) +
+  stat_ellipse(aes(group = ASE), type = "norm", level = 0.95, linewidth = 0.8) +
+  labs(
+    x = paste0("PCoA1 (", round(var_expl[1], 1), "%)"),
+    y = paste0("PCoA2 (", round(var_expl[2], 1), "%)"),
+    color = "ASE"
+  ) +
+  theme_classic()
+
 
 
 ##Ordination, using both bray and jaccard##
