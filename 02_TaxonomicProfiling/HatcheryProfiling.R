6e04b8a52b70d8d4dbfd8fb0fb9939b4a91ab9f6
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2026-02-16 09:05:21 -0800

    Update HatcheryProfiling.R

--- 02_TaxonomicProfiling/HatcheryProfiling.R
+++ 02_TaxonomicProfiling/HatcheryProfiling.R
@@ -13,10 +13,11 @@ ps_new@sam_data$hatchery <- factor(ps_new@sam_data$hatchery, levels = desired_fa
 
 #Genus level
 myPal <- tax_palette(data = ps_new, rank = "Genus", n = 15, pal = "greenArmytage",add = c(Other = "white"))
-
-#Genus level
 comp_barplot(ps=ps_new,tax_level = "Genus", n_taxa = 15, bar_width = 0.8,palette = myPal,merge_other=FALSE,facet_by = "hatchery") +labs(x = "", y = "Relative Abundance")+theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1))+theme(axis.text.x = element_blank())
 
+#Phylum level
+myPal <- tax_palette(data = ps_new, rank = "Phylum", n = 15, pal = "greenArmytage",add = c(Other = "white"))
+comp_barplot(ps=ps_new,tax_level = "Phylum", n_taxa = 15, bar_width = 0.8,palette = myPal,merge_other=FALSE,facet_by = "hatchery") +labs(x = "", y = "Relative Abundance")+theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1))+theme(axis.text.x = element_blank())
 
 
 
