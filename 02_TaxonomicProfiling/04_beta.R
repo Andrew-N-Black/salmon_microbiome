f58733faf7474b672dfdf0a49d07637084a30ea7
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2026-02-16 16:02:29 -0800

    Update 04_beta.R

--- 02_TaxonomicProfiling/04_beta.R
+++ 02_TaxonomicProfiling/04_beta.R
@@ -38,11 +38,17 @@ ggsave("~/atchison_m.10_pcoa.svg")
 
 
 ##Ordination, using both bray and jaccard##
-#Hatchery and ASE Bray
+#Hatchery by ASE Bray-BRAY
 plot_ordination(ps_rarefied, ordinate(ps_rarefied, "MDS",distance="bray"))  +geom_vline(xintercept = 0, linetype = "dashed", color = "grey50")+geom_hline(yintercept = 0, linetype = "dashed", color = "grey50")+ geom_point(size = 4,aes(shape=ASE,color=hatchery,fill=hatchery)) +stat_ellipse(aes(group = hatchery,color=hatchery), type = "norm", level = 0.95, linewidth = 0.8) +labs(title="Bray Curtis") +
     theme_classic()+scale_fill_brewer(palette = "Dark2")+scale_color_brewer(palette = "Dark2")
 ggsave("~/Figure_3a.svg")
 
+#ASE by Hatchery Bray-BRAY
+plot_ordination(ps_rarefied, ordinate(ps_rarefied, "MDS",distance="bray"))  +geom_vline(xintercept = 0, linetype = "dashed", color = "grey50")+geom_hline(yintercept = 0, linetype = "dashed", color = "grey50")+ geom_point(size = 4,aes(shape=hatchery,color=ASE,fill=ASE)) +stat_ellipse(aes(group = ASE,color=ASE), type = "norm", level = 0.95, linewidth = 0.8) +labs(title="Bray Curtis") +
+    theme_classic()+scale_fill_brewer(palette = "Dark2")+scale_color_brewer(palette = "Dark2")+scale_shape_manual(values = c(20,2,3,4,8,6))
+ggsave("~/Figure_3c.svg")
+
+#Hatchery and ASE Bray-JACCARD
 plot_ordination(ps_rarefied, ordinate(ps_rarefied, "MDS",distance="jaccard"))  +geom_vline(xintercept = 0, linetype = "dashed", color = "grey50")+geom_hline(yintercept = 0, linetype = "dashed", color = "grey50")+ geom_point(size = 4,aes(shape=ASE,color=hatchery,fill=hatchery)) +stat_ellipse(aes(group = hatchery,color=hatchery), type = "norm", level = 0.95, linewidth = 0.8) +labs(title="jaccard") +
     theme_classic()+scale_fill_brewer(palette = "Dark2")+scale_color_brewer(palette = "Dark2")
 ggsave("~/rarefied_jaccard.svg")
