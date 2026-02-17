f4bd6dc96dd7d234636029e0f1b05e2c47df062a
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2026-02-16 16:16:47 -0800

    Update 02_Alpha.R

--- 02_TaxonomicProfiling/02_Alpha.R
+++ 02_TaxonomicProfiling/02_Alpha.R
@@ -7,11 +7,12 @@ library(dplyr)
 library(reshape2) 
 
 #Alpha Diversity:
+#By ASE
+p<-plot_richness(ps_rarefied, x="ASE",color="ASE" ,measures=c("Observed", "Shannon"))+ theme_classic(base_size = 14) 
+p + geom_boxplot(size=.5)+scale_color_brewer(palette = "Dark2")+theme(axis.title.x = element_blank(),axis.text.x = element_blank(), axis.ticks.x = element_blank())+geom_jitter(width=0.1)+scale_fill_brewer(palette = "Dark2")
 
-p<-plot_richness(ps_rarefied, x="ASE",color="ASE" ,measures=c("Observed", "Shannon"))+ theme_q2r() 
-p + geom_boxplot(size=1)+scale_color_brewer(palette = "Dark2")+theme(axis.title.x = element_blank(),axis.text.x = element_blank(), axis.ticks.x = element_blank())
+ ggsave("~/Figure_3E.svg")
 
- ggsave("~/Figure_2.svg")
 
 #Significance test for ASE
 observed<-p$data[1:61,]
@@ -19,3 +20,7 @@ kruskal.test(value ~ ASE, data = observed)
 
 shannon<-p$data[62:122,]
 kruskal.test(value ~ ASE, data = shannon)
+
+
+p<-plot_richness(ps_rarefied, x="hatchery",color="hatchery" ,measures=c("Observed", "Shannon"))+ theme_classic(base_size = 14) 
+p + geom_boxplot(size=.5)+scale_color_brewer(palette = "Dark2")+theme(axis.title.x = element_blank(),axis.text.x = element_blank(), axis.ticks.x = element_blank())+geom_jitter(width=0.1)+scale_fill_brewer(palette = "Dark2")
