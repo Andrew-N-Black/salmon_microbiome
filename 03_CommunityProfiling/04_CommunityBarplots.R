b894845bf492403b23ade8515c195f325cc62450
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2026-03-11 15:11:39 -0700

    Update 04_CommunityBarplots.R

--- 03_CommunityProfiling/04_CommunityBarplots.R
+++ 03_CommunityProfiling/04_CommunityBarplots.R
@@ -3,9 +3,7 @@ library(microbiome)
 library(phyloseq)
 
 #Remove very low abundant ASVs before generating barplots
-ps_min.10 <- microViz::tax_filter(hr_phyloseq, min_prevalence = 0.10)
-#Proportional min_prevalence given: 0.1 --> min 6/60 samples.
-ps_rel = microbiome::transform(ps_min.10, "compositional")
+ps_rel = microbiome::transform(ps.tax.filtered, "compositional")
 
 #Fix taxa names
 ps_new <-ps_rel  %>%  tax_fix()
@@ -15,10 +13,15 @@ myPal <- tax_palette(data = ps_new, rank = "Genus", n = 10, pal = "greenArmytage
 desired_facet_order <- c("minter_creek","white_river", "south_santiam", "sandy", "willamette","round_butte")
 ps_new@sam_data$hatchery <- factor(ps_new@sam_data$hatchery, levels = desired_facet_order)
 
-#Genus level (Figure 3c)
-myPal <- tax_palette(data = ps_new, rank = "Genus", n = 15, pal = "greenArmytage",add = c(Other = "white"))
-comp_barplot(ps=ps_new,tax_level = "Genus", n_taxa = 15, bar_width = 0.8,palette = myPal,merge_other=FALSE,facet_by = "hatchery") +labs(x = "", y = "Relative Abundance")+theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1))+theme(axis.text.x = element_blank())
-
 #Phylum level (Figure 3b)
 myPal <- tax_palette(data = ps_new, rank = "Phylum", n = 15, pal = "greenArmytage",add = c(Other = "white"))
-comp_barplot(ps=ps_new,tax_level = "Phylum", n_taxa = 15, bar_width = 0.8,palette = myPal,merge_other=FALSE,facet_by = "hatchery") +labs(x = "", y = "Relative Abundance")+theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1))+theme(axis.text.x = element_blank())
+comp_barplot(ps=ps_new,tax_level = "Phylum", n_taxa = 15, bar_width = 0.8,palette = myPal,merge_other=FALSE,facet_by = "hatchery") +labs(x = "", y = "Relative Abundance")+theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1))+theme(axis.text.x = element_blank())+theme(axis.ticks.x = element_blank())
+ggsave("~/Figure_3b.svg", width = 8, height = 5)
+
+
+#Genus level (Figure 3c)
+myPal <- tax_palette(data = ps_new, rank = "Genus", n = 15, pal = "greenArmytage",add = c(Other = "white"))
+comp_barplot(ps=ps_new,tax_level = "Genus", n_taxa = 15, bar_width = 0.8,palette = myPal,merge_other=FALSE,facet_by = "hatchery") +labs(x = "", y = "Relative Abundance")+theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1))+theme(axis.text.x = element_blank())+theme(axis.ticks.x = element_blank())
+
+ggsave("~/Figure_3c.svg", width = 8, height = 5)
+
