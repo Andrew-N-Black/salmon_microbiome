a2be25d63f4c84af7aaf9622147fa738bb9d0d12
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2026-03-11 11:08:43 -0700

    Update 02_HatcheryDepth.R

--- 03_CommunityProfiling/02_HatcheryDepth.R
+++ 03_CommunityProfiling/02_HatcheryDepth.R
@@ -2,9 +2,9 @@ library(phyloseq)
 library(ggplot2)
 
 #Summarize read depth / sample and save in metadata
-read_counts <- sample_sums(hr_phyloseq)
-sample_data(hr_phyloseq)$TotalReads <- sample_sums(hr_phyloseq)
-metadata<-sample_data(hr_phyloseq)
+read_counts <- sample_sums(ps.tax.filtered)
+sample_data(ps.tax.filtered)$TotalReads <- sample_sums(ps.tax.filtered)
+metadata<-sample_data(ps.tax.filtered)
 
 #Reorder to set order of hatcheries
 desired_order <- c("minter_creek","white_river", "south_santiam", "sandy", "willamette","round_butte")
@@ -16,6 +16,7 @@ ggplot(metadata, aes(y =TotalReads, x=hatchery)) +
     labs(x = "",
          y = "log(Total Reads / Sample)") +
     theme_classic(base_size = 14)+geom_boxplot(aes(x=hatchery, y=TotalReads), width=0.2,fill="grey")+geom_jitter(aes(x=hatchery, y=TotalReads), width=0.1)+theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1))+scale_y_log10()
+ggsave("~/Figure_3a.svg", width = 8, height = 5)
 
 #Statistical test for differences in read depth
 kruskal.test(TotalReads ~ hatchery, data = metadata)
